5d038728c3f70f0aabff4d05ce8cb1c91f01a79e
Jos Codes <joselineruiz917@gmail.com>
2024-10-28 23:45:59 -0500

    first commit

---
+++ variables.js
@@ -0,0 +1,22 @@
+// dynamic typing: refers to ability to initialize variable to data type
+let firstName = 'Joseline';
+console.log(typeof firstName);
+
+firstName = 100;
+console.log(typeof firstName);
+
+firstName = true;
+console.log(typeof firstName);
+
+// object: data type in js that allows you to sort key value pairs, they're a reference type
+let course = {
+    name: 'Javascript refresh',
+    hours: 3
+};
+console.log(course.name);
+
+// better way to write object
+course.name = 'javascript refresh';
+console.log(course.name);
+
+// 27min
